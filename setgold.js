65c5f0511ecc25c55c654cceaf89e58ca76df87e
Vafor <68476283+v4for@users.noreply.github.com>
2024-10-30 09:50:36 -0600

    Update setgold.js

--- setgold.js
+++ setgold.js
@@ -1,3 +1,10 @@
 gold = prompt("How much gold)
 
-stateNode.setState({ gold, gold2: gold });
+let { stateNode } = Object.values((function react(r = document.querySelector("body>div")) { return Object.values(r)[1]?.children?.[0]?._owner.stateNode ? r : react(r.querySelector(":scope>div")) })())[1].children[0]._owner;
+        stateNode.setState({ gold, gold2: gold });
+        stateNode.props.liveGameController.setVal({
+            path: "c/".concat(stateNode.props.client.name),
+            val: {
+                b: stateNode.props.client.blook,
+                g: gold
+            }
